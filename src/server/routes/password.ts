c14c01e7b989d64a3c2bc7793ad556ce47a30a02
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2025-12-03 17:10:40 +0100

    fix: Correct db import in password route (default import)

--- src/server/routes/password.ts
+++ src/server/routes/password.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { db } from "../db";
+import db from "../db";
 import bcrypt from "bcryptjs";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
